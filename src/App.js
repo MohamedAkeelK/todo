df3c1087c62d65802c4d6ff9ed82dda3779234a0
unknown <Mohamedakeelkhan@gmail.com>
2020-08-08 03:27:11 -0400

    got the add Task button working partially

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class toDoList extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      todos: [],
+      currentToDo: "",
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.addItem = this.addItem.bind(this);
+  }
+
+  handleChange = event => {
+    // console.log(event.target.value)
+    this.setState({ 
+      currentToDo: event.target.value,
+    })
+  };
+
+  addItem = event => {
+    event.preventDefault();
+    // console.log("additem Method fired")
+    const newItem = this.state.currentToDo;
+    // console.log(newItem);
+    this.state.todos.push(newItem)
+    console.log(this.state)
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <form onSubmit={this.addItem}>
+          <label htmlFor="taskName">Task Name:</label>
+          <input onChange={this.handleChange} name="taskName" type="text" placeholder="Add todo here!"/>
+          <button type="submit">Add Task</button>
+        </form>
+
+        <ul>
+         {this.state.todos.map(taskName => <li key={taskName}>{taskName}</li>)}
+        </ul>
+
+      </div>
+    )
+  }
 }
 
-export default App;
+export default toDoList;
